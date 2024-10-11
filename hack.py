71ee87c0938a1ce08c05c91e911983cb7d8efd03
Egrillet89 <egrillet.valera@gmail.com>
2024-10-10 20:39:13 -0400

    feat: py test

---
+++ hack.py
@@ -0,0 +1,17 @@
+def suma():
+    return 2
+
+def restar():
+    return 1
+
+
+def lista():
+    return ["foo","bar","charlie"]
+
+
+def texto(txt):
+    return txt
+
+
+def texto_upper(txt):
+    return txt.upper()
